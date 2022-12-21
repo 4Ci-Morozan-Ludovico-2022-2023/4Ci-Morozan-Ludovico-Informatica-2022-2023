ccf90d610e8f231bb3f11344ec360f1043caa660
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2022-12-22 00:17:24 +0100

    Create LavatriceTest.java

---
+++ 2022-2023/bin/lavatrice/VER01/LavatriceTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class LavatriceTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Lavatrice lava = new Lavatrice("samsung", "D-2022", 200.0);
+        System.out.print(lava.toString());
+    }
+}
