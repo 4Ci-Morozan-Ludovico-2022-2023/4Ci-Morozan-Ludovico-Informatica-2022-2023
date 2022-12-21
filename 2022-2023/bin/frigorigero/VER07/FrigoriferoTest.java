c6d6791bf1a066c0a7f6d35133f8396defed32e1
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2022-12-22 00:09:36 +0100

    Create FrigoriferoTest.java

---
+++ 2022-2023/bin/frigorigero/VER07/FrigoriferoTest.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class FrigoriferoTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Frigorifero frigo = new Frigorifero("Samsung", "D2022", 30.0);
+        System.out.print("\n"+frigo.toString());
+        //02
+        frigo.accendi();
+        System.out.print("\n" + frigo.toString());
+        frigo.spegni();
+        System.out.print("\n" + frigo.toString());
+        //03
+        frigo.setMarca("samsung");
+        System.out.print("\n" + frigo.getMarca());
+        //04
+        frigo.setModello("f2000");
+        System.out.print("\n" + frigo.getModello());
+        //05
+        frigo.setlitriFrigo(100);
+        System.out.print("\n" + frigo.getlitriFrigo());
+        //06
+        frigo.setlitriFrigo(17);
+        System.out.print("\n" + frigo.getlitriFrigo());
+        //07
+        frigo.setPrezzo(700.0);
+        System.out.print("\n" + frigo.getPrezzo());
+    }
+}
