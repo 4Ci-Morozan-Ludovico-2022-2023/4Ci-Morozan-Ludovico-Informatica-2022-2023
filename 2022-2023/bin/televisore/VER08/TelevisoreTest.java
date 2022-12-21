89b313b547dcad7ea736f092c2489f1019707c5c
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2022-12-22 00:32:44 +0100

    Create TelevisoreTest.java

---
+++ 2022-2023/bin/televisore/VER08/TelevisoreTest.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class TelevisoreTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Televisore tv = new Televisore(27, "8k", "black");
+        System.out.print(tv.toString());
+        //
+        tv.accendi();
+        System.out.print("\n"+ tv.toString());
+        tv.spegni();
+        System.out.print("\n"+ tv.toString());
+        //
+        tv.setPollici(27);
+        System.out.print("\n\n"+ tv.getPollici());
+        //
+        tv.setSchermo("8k");
+        System.out.print("\n\n"+ tv.getSchermo());
+        //
+        tv.setColore("black");
+        System.out.print("\n\n"+ tv.getColore());
+        //
+        tv.setCanale(41);
+        System.out.print("\n\n"+ tv.getCanale());
+        tv.aumentaCanale();
+        System.out.print("\n"+ tv.getCanale());
+        tv.diminuisciCanale();
+        System.out.print("\n"+ tv.getCanale());
+        //
+        tv.setVolume(33);
+        System.out.print("\n\n"+ tv.getVolume());
+        tv.aumentaVolume();
+        System.out.print("\n"+ tv.getVolume());
+        tv.diminuisciVolume();
+        System.out.print("\n"+ tv.getVolume());
+        //
+        tv.setLuminos(50);
+        System.out.print("\n\n"+ tv.getLuminos());
+        tv.aumentaLuminos();
+        System.out.print("\n"+ tv.getLuminos());
+        tv.diminuisciLuminos();
+        System.out.print("\n"+ tv.getLuminos());
+    }
+}
