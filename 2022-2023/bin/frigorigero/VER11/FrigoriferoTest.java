2a918c5797e9faa0bc3d7395b7e269cac97c106e
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2022-12-22 00:13:14 +0100

    Create FrigoriferoTest.java

---
+++ 2022-2023/bin/frigorigero/VER11/FrigoriferoTest.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class FrigoriferoTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Frigorifero frigo = new Frigorifero("Samsung", "D2022", 30.0);
+        Frigorifero frigo2 = new Frigorifero(frigo);
+        System.out.print("\n"+frigo.toString());
+        //02
+        frigo.accendi();
+        System.out.print("\n" + frigo.toString());
+        frigo.spegni();
+        System.out.print("\n" + frigo.toString());
+        //03
+        frigo.setMarca("samsung");
+        System.out.print("\n" + frigo.getMarca());
+        //04
+        frigo.setModello("f2000");
+        System.out.print("\n" + frigo.getModello());
+        //05
+        frigo.setlitriFrigo(100);
+        System.out.print("\n" + frigo.getlitriFrigo());
+        //06
+        frigo.setlitriFrigo(17);
+        System.out.print("\n" + frigo.getlitriFrigo());
+        //07
+        frigo.setPrezzo(700.0);
+        System.out.print("\n" + frigo.getPrezzo());
+        //08
+        frigo.setconsumokwat(2);
+        System.out.print("\n" + frigo.getconsumokwat());
+        //09
+        frigo.settemperaturaFrigo(1);
+        System.out.print("\n" + frigo.gettemperaturaFrigo());
+        //10
+        frigo.settemperaturaFrizer(-2);
+        System.out.print("\n" + frigo.gettemperaturaFrizer());
+        //11
+        System.out.print("\n" + "\n" + frigo2);
+    }
+}
