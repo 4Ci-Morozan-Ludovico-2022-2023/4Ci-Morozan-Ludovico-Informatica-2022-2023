eb2ce6f6ec4d25cb5881523d6f4b9332a901020b
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2022-12-22 00:30:09 +0100

    Create Televisore.java

---
+++ 2022-2023/bin/televisore/VER04/Televisore.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Televisore
+{
+    private int pollici = 0;
+    private String schermo = "anonimo";
+    private String colore = "anonimo";
+    private int canale = 0;
+    private int volume = 0;
+    private int luminos = 50;
+    private boolean acceso = false;
+    public Televisore(int pollici, String schermo, String colore){
+         if(pollici>0){
+             this.pollici=pollici;
+         }
+         if(schermo!=null){
+             this.schermo=schermo;
+         }
+         if(colore!=null){
+             this.colore=colore;
+         }
+    }
+    //
+     public void accendi(){
+        this.acceso = true;
+    }
+    public void spegni(){
+        this.acceso = false;
+    }
+    //
+    public int getPollici(){
+        return this.pollici;
+    }
+    public void setPollici(int p){
+        if(p>0){
+            this.pollici=p;
+        }
+    }
+    //
+     public String getSchermo(){
+        return this.schermo;
+    }
+    public void setSchermo(String s){
+        if(s!=null){
+            this.schermo=s;
+        }
+    }
+    //
+    public String toString(){
+        String out="";
+        out="\nPollici: "+this.pollici;
+        out+="\nSchermo: "+this.schermo;
+        out+="\nColore: "+this.colore;
+        out+="\nCanale: "+this.canale;
+        out+="\nVolume: "+this.volume;
+        out+="\nLuminosità: "+this.luminos;
+        if(this.acceso){
+            out+="\nAcceso";
+        }else{
+            out+="\nSpento";
+        }
+        return out;
+    }
+}
