55f2e44fdabb9ca0d0669641b83f9e498f2a0220
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2023-01-23 23:44:24 +0100

    Create Contatoregas.java

---
+++ 2022-2023/bin/contatoregas/VER04/Contatoregas.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class Contatoregas{
+    private String marca;
+    private String modello;
+    private String classe;
+    private int annodifabbr;
+    private double portataminima;
+    private double portatamassima;
+    private double consumo;
+    private boolean acceso;
+    public Contatoregas (String m, String mod, String c){
+        if (m!=null){
+            this.marca = m;
+        }else{
+            this.marca = "anonimo";
+        }
+        if (mod!=null){
+            this.modello = mod;
+        }else{
+            this.modello = "anonimo";
+        }
+        if (c!=null){
+            this.classe = c;
+        }else{
+            this.classe = "anonimo";
+        }
+        this.annodifabbr = 0;
+        this.portataminima = 0.0;
+        this.portatamassima = 0.0;
+        this.consumo = 0.0;
+        this.acceso = false;
+    }
+    //02
+    public void accendi(){
+        this.acceso = true;
+    }
+    public void spegni(){
+        this.acceso = false;
+    }
+    //03
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String m){
+        if(m != null){
+            this.marca = m;
+        }
+    }
+    //04
+    public String getModello(){
+        return this.modello;
+    }
+    public void setModello(String mod){
+        if(mod != null){
+            this.modello = mod;
+        }
+    }
+    //
+    public String toString(){
+        String out = "Marca: " + this.marca + "\n";
+        out += "Modello: " + this.modello + "\n";
+        out += "Classe: " + this.classe + "\n";
+        out += "Anno di fabbricazione: " + this.annodifabbr + "\n";
+        out += "Portata minima: " + this.portataminima + "\n";
+        out += "Portata massima: " + this.portatamassima + "\n";
+        out += "Consumo: " + this.consumo + "\n";
+        if(acceso){
+            out += "è acceso" + "\n";
+        }else{
+            out += "è spento" + "\n";
+        }
+        return out;
+    }
+}
