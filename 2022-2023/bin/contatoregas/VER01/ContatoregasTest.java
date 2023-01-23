ab279bf3151576006d14a0aad92b457ff65cacba
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2023-01-23 23:35:09 +0100

    Create ContatoregasTest.java

---
+++ 2022-2023/bin/contatoregas/VER01/ContatoregasTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ContatoregasTest{
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        Contatoregas cont = new Contatoregas("samsung", "xvnd", "F51");
+        System.out.print("\n"+cont.toString());
+    }
+}
