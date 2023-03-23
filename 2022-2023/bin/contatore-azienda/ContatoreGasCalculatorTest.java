365c5b99c8ac2d39b77f5bc2941c7fa9d924aa0d
4Ci-Morozan-Ludovico-2022-2023 <117983438+4Ci-Morozan-Ludovico-2022-2023@users.noreply.github.com>
2023-03-23 22:51:13 +0100

    Create ContatoreGasCalculatorTest.java

---
+++ 2022-2023/bin/contatore-azienda/ContatoreGasCalculatorTest.java
@@ -0,0 +1,6 @@
+public class ContatoreGasCalculatorTest{
+    public static void main(String[] args) {
+        ContatoreGasCalculator calculator = new ContatoreGasCalculator();
+        calculator.start();
+    }
+}
